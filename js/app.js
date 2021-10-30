0cfdb736031db6051d29831873b659e30d0f0c55
Nikka <nikkathehope@example.com>
2021-10-30 15:49:35 +0900

    updated

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 const nav = document.createElement("div");
 nikkaFunc.makeClasses(["flex__container","row"],nav);
 
+const header = document.createElement("div");
+header.innerText = "HEADER";
+nav.append(header);
+
 menus.forEach( menu=> {
     const div = document.createElement("div");
     div.innerText = menu;
