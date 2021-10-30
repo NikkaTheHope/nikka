e8625f6a6e2577c7bfa467c247ec1683408506f8
Nikka <nikkathehope@gmail.com>
2021-10-30 20:15:07 +0900

    add timer/date = nikkaTime

--- nikka.js
+++ nikka.js
@@ -11,5 +11,30 @@ const nikkaFunc = {
     }
 };
 
+const nikkaTime = {
+    normalTime : function(){
+        const date = new Date();
+        return `${String(date.getHours()).padStart(2,"0")} : ${String(date.getMinutes()).padStart(2,"0")} : ${String(date.getSeconds()).padStart(2,"0")}`;
+    },
+    normalDate : function(type){
+        const date = new Date();
+        let res = null;
+        switch(type){
+            case "ko" :
+                res = `${String(date.getDate()).padStart(2,"0")}년 ${String((date.getMonth()+1)).padStart(2,"0")}월 ${String(date.getDate()).padStart(2,"0")}일`;
+            break;
+            case "-" :
+                res = `${String(date.getDate()).padStart(2,"0")}-${String((date.getMonth()+1)).padStart(2,"0")}-${String(date.getDate()).padStart(2,"0")}`;
+            break;
+            case "/" :
+                res = `${String(date.getDate()).padStart(2,"0")}/${String((date.getMonth()+1)).padStart(2,"0")}/${String(date.getDate()).padStart(2,"0")}`;
+            break;
+            case "." :
+                res = `${String(date.getDate()).padStart(2,"0")}.${String((date.getMonth()+1)).padStart(2,"0")}.${String(date.getDate()).padStart(2,"0")}`;
+            break;
+        }
+    }
+};
+
 const body = document.querySelector("body");
 nikkaFunc.makeClasses(["flex__container"],body);
