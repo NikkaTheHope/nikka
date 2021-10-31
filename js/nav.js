ce53e08f4d57c4c443e5e935be178247056dc31c
Nikka <nikkathehope@gmail.com>
2021-10-31 16:35:27 +0900

    j

--- js/nav.js
+++ js/nav.js
@@ -1,11 +1,56 @@
-const drawNav = {
+const nav = document.createElement("div");
+nikkaFunc.makeClasses(["flex__container","card__normal","row"],nav);
+const header = document.createElement("header");
+header.innerText = "HEADER";
+header.addEventListener("click",function(){nikkaFunc.makeBtnShow(nav.children,header,"display");});
+nav.append(header);
 
-    indexNav : function(){
-        const nav = document.createElement("div");
-        nikkaFunc.makeClasses(["flex__container","row","scroll-box-x","card-normal"],nav);
-        body.append(nav);
+const archive = {
+    indexNav : {
+        study : {
+            id : 'studyNav',
+            name : "Study"
+        },
+        menu_02 : {
+            id : '002',
+            name : "menu 02"
+        },
+        menu_03 : {
+            id : '003',
+            name : "menu 03"
+        }
+    },
+    studyNav : {
+        javascript : {
+            id : '001_001',
+            name : 'Javascript | 자바스크립트'
+        },
+        linux : {
+            id : '001_002',
+            name : 'Linux | 리눅스'
+        }
     }
-
 };
 
-drawNav.indexNav();
+const initNavs = {
+    navMain : function(){
+        const arr = [];
+        Object.keys(archive.indexNav).forEach( i => {
+            const div = document.createElement("div");
+            div.innerText = archive.indexNav[i].name;
+            nikkaFunc.makeClasses(["btn"],div);
+            nav.append(div);
+            arr.push(div);
+            div.addEventListener("click",this[archive.indexNav[i].id]);
+        });
+        body.append(nav);
+        return arr;
+    },
+    studyNav : function(){
+        const arr = [];
+        console.log("aaaa");
+    }
+};
+
+const navMain = initNavs.navMain();
+console.log(navMain);
--- js/var.js
+++ js/var.js
@@ -1,19 +1,1 @@
-//Load menus from DB
-const archive = {
-    STUDY : {
-        alt : "Study",
-        list : {
-            js : {
-                name : 'javascript'
-            },
-            linux : {
-                name : 'linux'
-            }
-        }
-    },
-    MENU02 :{
-
-    }
-};
-
 const home_addr = "https://nikkathehope.github.io/nikka/";
