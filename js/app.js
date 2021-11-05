62e78ec3b5848f227a5800a220e251494c068460
Nikka <nikkathehope@gmail.com>
2021-11-05 12:46:57 +0900

    modified

--- js/app.js
+++ js/app.js
@@ -1,11 +1,20 @@
 menus.forEach(menu=>{
-    nav.appendChild(createNavBt(menu));
+    const data = createNavBt(menu)
+    data.addEventListener("click", changeContent);
+    nav.appendChild(data);
 });
 body.appendChild(nav);
+
+body.appendChild(content);
 
 function createNavBt(str){
     const div = document.createElement("div");
     vjs.addclasses(["nav_bt"],div);
+    div.style.border = "solid var(--base-c ) 3px";
     div.innerText = str;
     return div;
+}
+
+function changeContent(e){
+    content.innerText = e.target.innerText;
 }
--- js/var.js
+++ js/var.js
@@ -3,4 +3,8 @@ vjs.addclasses(["flex_container","f_col"],body);
 
 const nav = document.createElement("div");
 vjs.addclasses(["nav","flex_container","f_row"],nav);
-const menus = ["a","b","c"];
+const menus = ["a","b","c","b","c","b","c","b","c","b","c","b","c","b","c","b","c","b","c","b","c","b","c","b","c","b","c","b","c","b","c","b","c"];
+
+const content = document.createElement("div");
+content.id = "content";
+content.style.height = (innerHeight*0.85)+"px";
